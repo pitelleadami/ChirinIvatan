3a61772395ac8b6db8d74aba24c3fe5e41ebb865
pitelleadami <pitelleadami1994@gmail.com>
2026-02-06 15:49:12 +0800

    chore: move Django apps under backend package

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
+    'dictionary',
+    'folklore',
+    'reviews',
 ]
 
 MIDDLEWARE = [
